95e217402a62eb0ba4102c0f41311fc2027e2d61
mcghieb <mcghie.blake@gmail.com>
2024-02-26 14:29:23 -0700

    extremely basic server/webpage running

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,24 @@
 import chess.*;
+import server.ChessServer;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            var port = 8080;
+            if (args.length > 1) {
+                port = Integer.parseInt(args[0]);
+            }
+
+            // IMPLEMENT CHESSSERVER
+            var server = new ChessServer().run(port);
+            port = server.port();
+            System.out.printf("Server started on port %d%n", port);
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+
     }
 }
